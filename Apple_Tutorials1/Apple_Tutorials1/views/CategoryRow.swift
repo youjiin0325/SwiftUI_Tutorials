0897c475782c11fe424c0719cba12cd5a78f325d
youjiin0325 <youjiin0325@gmail.com>
2024-03-17 22:41:52 +0900

    Feat: 랜드마크 이미지를 리스트별 Feature 별로 탭류로 나누고 선택하여 보기가능

---
+++ Apple_Tutorials1/Apple_Tutorials1/views/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Apple_Tutorials1
+//
+//  Created by 유지인 on 3/17/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
