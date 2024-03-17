0b1359e74218095b680f850d83cad76d69adcf95
youjiin0325 <youjiin0325@gmail.com>
2024-03-17 23:36:01 +0900

    Feat: 유저프로파일 생성해서 뱃지랑 간단한 정보 그리고 최근 하이킹 정보 표시

---
+++ Apple_Tutorials1/Apple_Tutorials1/Model/ Profile.swift
@@ -0,0 +1,8 @@
+//
+//   Profile.swift
+//  Apple_Tutorials1
+//
+//  Created by 유지인 on 3/17/24.
+//
+
+import Foundation
---
+++ Apple_Tutorials1/Apple_Tutorials1/Model/ProfileHost.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHost.swift
+//  Apple_Tutorials1
+//
+//  Created by 유지인 on 3/17/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
