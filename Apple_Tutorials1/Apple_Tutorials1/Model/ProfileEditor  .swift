ce4c8c1a2a17781ccb099cd529c359aac7f267f0
youjiin0325 <youjiin0325@gmail.com>
2024-03-21 21:24:04 +0900

    Feat: 프로필 페이지에 에디터 모드 버튼을 추가

---
+++ Apple_Tutorials1/Apple_Tutorials1/Model/ProfileEditor  .swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor  .swift
+//  Apple_Tutorials1
+//
+//  Created by 유지인 on 3/21/24.
+//
+
+import SwiftUI
+
+struct ProfileEditor__: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor__()
+}
